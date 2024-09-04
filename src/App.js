14b2f7f91b7d1f410077aff8dd21a0f5f20a6d60
Valid Rabiee <validrabieejs@gmail.com>
2024-09-04 20:09:12 +0330

    add WelcomeSection component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/layout/Header";
+import WelcomeSection from "./components/layout/WelcomeSection";
 
 const App = () => {
   return (
     <>
       <Header />
+      <WelcomeSection />
     </>
   );
 }
---
+++ src/components/layout/WelcomeSection.js
@@ -0,0 +1,21 @@
+import classes from './WelcomeSection.module.css';
+import buttonClasses from '../../styles/buttons.module.css';
+import banner from '../../assets/images/logo_large.png';
+
+const WelcomeSection = () => {
+  return (
+    <section className={classes.section}>
+      <div>
+        <h1>با حساب و کتاب خرج کن</h1>
+        <p>پول های خود را به راحتی مدیریت کنید</p>
+        <div>
+          <input type='email' placeholder="ایمیل خود را وارد کنید" />
+          <button className={`${buttonClasses.button} ${buttonClasses.primary}`}>ثبت نام</button>
+        </div>
+      </div>
+      <img src={banner} alt="پول افزار" />
+    </section>
+  );
+}
+ 
+export default WelcomeSection;
