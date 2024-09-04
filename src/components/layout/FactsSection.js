98f18f7017d3d1bee1737dd72eac8a45ce56a1da
Valid Rabiee <validrabieejs@gmail.com>
2024-09-05 03:22:24 +0330

    Remove unnecessary comment

--- src/components/layout/FactsSection.js
+++ src/components/layout/FactsSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './FactsSection.module.css'; // استایل مرتبط با این بخش
+import styles from './FactsSection.module.css';
 
 const FactsSection = () => {
   const facts = [
