60819e13c6014d063593ad0445a2f4bc59951127
Valid <dev.valid7@gmail.com>
2024-11-28 20:09:42 +0330

    Update AccountCard UI to show currency

--- src/components/AccountCard.js
+++ src/components/AccountCard.js
@@ -1,11 +1,11 @@
 import classes from './AccountCard.module.css';
 
-const AccountCard = ({ name, number, balance }) => {
+const AccountCard = ({ name, number, balance, currency }) => {
   return (
     <div className={classes.card}>
       <span className={classes.name}>{name}</span>
       <span className={classes.number}>{number}</span>
-      <span className={classes.balance}>{balance} تومان</span>
+      <span className={classes.balance}>{currency === 'USD' ? '$' : ''} {balance} {currency === 'IRT' ? 'تومان' : ''}</span>
     </div>
   );
 };
--- src/components/AccountSlider.js
+++ src/components/AccountSlider.js
@@ -11,16 +11,16 @@ import AccountCard from './AccountCard';
 
 
 const accounts = [
-  { name: 'حساب 1', number: '1234567890', balance: '100,000', key: 'a1' },
-  { name: 'حساب 2', number: '1234567890', balance: '100,000', key: 'a2' },
-  { name: 'حساب 3', number: '1234567890', balance: '100,000', key: 'a3' },
-  { name: 'حساب 4', number: '1234567890', balance: '100,000', key: 'a4' },
-  { name: 'حساب 5', number: '1234567890', balance: '100,000', key: 'a5' },
-  { name: 'حساب 6', number: '1234567890', balance: '100,000', key: 'a6' },
-  { name: 'حساب 7', number: '1234567890', balance: '100,000', key: 'a7' },
-  { name: 'حساب 8', number: '1234567890', balance: '100,000', key: 'a8' },
-  { name: 'حساب 9', number: '1234567890', balance: '100,000', key: 'a9' },
-  { name: 'حساب 10', number: '1234567890', balance: '100,000', key: 'a10' },
+  { name: 'حساب 1', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a1' },
+  { name: 'حساب 2', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a2' },
+  { name: 'نوبیتکس', number: 'LqYrg', balance: '100', currency: 'USD', key: 'a3' },
+  { name: 'حساب 4', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a4' },
+  { name: 'حساب 5', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a5' },
+  { name: 'حساب 6', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a6' },
+  { name: 'حساب 7', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a7' },
+  { name: 'حساب 8', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a8' },
+  { name: 'حساب 9', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a9' },
+  { name: 'حساب 10', number: '1234567890', balance: '100,000', currency: 'IRT', key: 'a10' },
 ];
 
 const AccountSlider = () => {
@@ -50,6 +50,7 @@ const AccountSlider = () => {
             name={account.name}
             number={account.number}
             balance={account.balance}
+            currency={account.currency}
           />
         </SwiperSlide>
       ))}
