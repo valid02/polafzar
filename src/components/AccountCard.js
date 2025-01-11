dd6f2cc8ee0c8e2642e0a9f2e27b5d0df83cfff1
Valid <dev.valid7@gmail.com>
2025-01-11 13:28:50 +0330

    refactor: improve formatting and add back button to ContentModal

--- src/components/AccountCard.js
+++ src/components/AccountCard.js
@@ -1,11 +1,14 @@
 import classes from './AccountCard.module.css';
 
-const AccountCard = ({ name, number, balance, currency }) => {
+const AccountCard = ({name, number, balance, currency}) => {
   return (
     <div className={classes.card}>
       <span className={classes.name}>{name}</span>
       <span className={classes.number}>{number}</span>
-      <span className={classes.balance}>{currency === 'USD' ? '$' : ''} {balance} {currency === 'IRT' ? 'تومان' : ''}</span>
+      <span className={classes.balance}>
+        {currency === 'USD' ? '$' : ''} {balance}
+        {currency === 'IRT' ? 'تومان' : ''}
+      </span>
     </div>
   );
 };
--- src/components/ContentModal.js
+++ src/components/ContentModal.js
@@ -1,19 +1,39 @@
 import ReactDOM from 'react-dom';
 import classes from './ContentModal.module.css';
-import { IoIosClose } from 'react-icons/io';
+import {IoIosArrowBack, IoIosClose} from 'react-icons/io';
 
-const ContentModal = ({ onClose, children, title, inset = '10rem auto auto 4rem', disableScroll = false  }) => {
+const ContentModal = ({
+  onClose,
+  children,
+  title,
+  inset = '10rem auto auto 4rem',
+  disableScroll = false,
+  showBackButton = false,
+  onBackClick
+}) => {
   return ReactDOM.createPortal(
     <div className={classes.backdrop} onClick={onClose}>
-      <section className={classes.section} style={{ inset }} onClick={e => e.stopPropagation()}>
+      <section
+        className={classes.section}
+        style={{inset}}
+        onClick={(e) => e.stopPropagation()}
+      >
         <header className={classes.header}>
-          <h2>{title}</h2>
-          <button className={classes['close-btn']} onClick={onClose}>
+          <button className={`${classes['close-btn']} ${classes['icon-btn']}`} onClick={onClose}>
             <IoIosClose />
           </button>
+          <h2>{title}</h2>
+          {showBackButton && (
+            <button className={`${classes['back-btn']} ${classes['icon-btn']}`} onClick={onBackClick}>
+              <IoIosArrowBack />
+            </button>
+          )}
         </header>
 
-        <div className={classes.content} style={{ overflowY: disableScroll ? 'hidden' : 'auto' }}>
+        <div
+          className={classes.content}
+          style={{overflowY: disableScroll ? 'hidden' : 'auto'}}
+        >
           {children}
         </div>
       </section>
