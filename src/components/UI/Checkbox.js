a59f605c6d4e7105b9ec70e9a9566cd55e062366
Valid <dev.valid7@gmail.com>
2025-01-15 14:33:42 +0330

    feat: enhance Checkbox component with checked state and change handler

--- src/components/UI/Checkbox.js
+++ src/components/UI/Checkbox.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Checkbox.css';
 
-const Checkbox = ({ id, size = "30px" }) => {
+const Checkbox = ({ id, size = "30px", checked, onChange, name }) => {
   return (
     <div className="checkbox-wrapper-23">
-      <input type="checkbox" id={id} />
+      <input type="checkbox" id={id} checked={checked} onChange={onChange} name={name} />
       <label htmlFor={id} style={{ '--size': size }}>
         <svg viewBox="0 0 50 50">
           <path d="M5 30 L 20 45 L 45 5"></path>
